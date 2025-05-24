f72d20ec23f9b5eda8a2e821439a78e4b3c7a894
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2025-05-24 18:13:16 +0200

    Add hipphipp

---
+++ src/main/java/no/noria/edb/kattis/HippHipp.java
@@ -0,0 +1,9 @@
+package no.noria.edb.kattis;
+
+public class HippHipp {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Hipp hipp hurra!");
+        }
+    }
+}
