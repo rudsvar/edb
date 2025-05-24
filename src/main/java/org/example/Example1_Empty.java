e0da8a5e2ad4f43f72b1cf09abc128e6816c2e0a
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2025-05-24 16:04:54 +0200

    Initial commit

---
+++ src/main/java/org/example/Example1_Empty.java
@@ -0,0 +1,26 @@
+// Code can be organized into *packages*, which is essentially just a folder.
+package org.example;
+
+/**
+ * Code is organized into *classes*, which is essentially a container of code and data.
+ * - Public means that it anyone can use it
+ * - Example1_Empty is the name of the class, usually the same as the file
+ */
+public class Example1_Empty { // Start of Example1_Empty
+
+    /**
+     * This is a method (function) called *main*.
+     * A method is just a piece of reusable code.
+     * It can receive inputs (String[] args) and produce output (void).
+     * *main* is special because it's the entrypoint of the program.
+     *
+     * A String is just text. The [] means it's an array (multiple Strings).
+     *
+     * *public* means the same as for classes.
+     * *static* means that only one variant of this method.
+     * *void* is the *return type* of the method.
+     */
+    public static void main(String[] args) {
+        // Code goes here
+    }
+} // End of Example1_Empty
---
+++ src/main/java/org/example/Example2_Print.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Example2_Print {
+    public static void main(String[] args) {
+        System.out.println("Hi!");
+    }
+}
