190922900a430258017429a2e9579210aa95f6a0
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2025-05-24 18:01:08 +0200

    Add first test script

--- kattest.sh
+++ kattest.sh
@@ -7,7 +7,7 @@ for f in $files; do
 
     # Prepare paths
     problem=$(basename "${f%.*}" | tr '[:upper:]' '[:lower:]')
-    echo "Testing" $problem
+    echo "🧪 Testing" $problem
     samples_dir="samples/kattis/$problem"
     mkdir -p "$samples_dir"
     samples_zip="$samples_dir/samples.zip"
@@ -15,24 +15,27 @@ for f in $files; do
 
     # Download samples if not already there
     if [ ! -f $samples_zip ]; then
-      echo "  Downloading samples_url to $samples"
+      echo "  🎣 Downloading samples_url to $samples"
       curl -s $samples_url > $samples_zip
-    else
-      echo "  Samples already at $samples_zip"
     fi
 
     # Unzip samples_url
     unzip -q -f $samples_zip -d $samples_dir
+  fi
 
     # Find input files
     input_files=$(find $samples_dir -type f -name "*.in")
     for input_file in $input_files; do
       input=$(cat $input_file)
-      output_file=$(echo $input_file | sed 's/.in/.ans/')
+      output_file=$(echo $input_file | sed 's/\.in/\.ans/')
       output_file_actual="$output_file.actual"
       output=$(cat $output_file)
       echo "$input" | java $f > "$output_file_actual"
-      git diff $output_file $output_file_actual
+      output_actual=$(cat "$output_file_actual")
+      if [[ "$output_actual" == "$output" ]]; then
+        echo "  ✅ $input_file"
+      else
+        echo "  ❌ $input_file"
+      fi
     done
-  fi
 done
