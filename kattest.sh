683e7329f58f81304325ec1d0889f6283ff41b57
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2025-05-24 17:44:09 +0200

    Update test script

--- kattest.sh
+++ kattest.sh
@@ -2,22 +2,37 @@
 
 files=$(find src/main/java/no/noria/edb/kattis -type f -name "*.java")
 
-if [[ $1 == "download" ]]; then
-  for f in $files; do
+for f in $files; do
+  if [[ $1 == "download" ]]; then
+
+    # Prepare paths
     problem=$(basename "${f%.*}" | tr '[:upper:]' '[:lower:]')
     echo "Testing" $problem
     samples_dir="samples/kattis/$problem"
     mkdir -p "$samples_dir"
-    samples="$samples_dir/samples.zip"
+    samples_zip="$samples_dir/samples.zip"
     samples_url="https://open.kattis.com/problems/$problem/file/statement/samples.zip"
-    if [ ! -f $samples ]; then
+
+    # Download samples if not already there
+    if [ ! -f $samples_zip ]; then
       echo "  Downloading samples_url to $samples"
-      curl -s $samples_url > $samples
+      curl -s $samples_url > $samples_zip
     else
-      echo "  Samples already at $samples"
+      echo "  Samples already at $samples_zip"
     fi
 
     # Unzip samples_url
-    unzip -q -f $samples -d $samples_dir
-  done
-fi
+    unzip -q -f $samples_zip -d $samples_dir
+
+    # Find input files
+    input_files=$(find $samples_dir -type f -name "*.in")
+    for input_file in $input_files; do
+      input=$(cat $input_file)
+      output_file=$(echo $input_file | sed 's/.in/.ans/')
+      output_file_actual="$output_file.actual"
+      output=$(cat $output_file)
+      echo "$input" | java $f > "$output_file_actual"
+      git diff $output_file $output_file_actual
+    done
+  fi
+done
