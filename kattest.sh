393a7bc7516528d7ccfc0edc15a0370c5e605c2c
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2025-05-24 18:12:21 +0200

    Add velkomin

--- kattest.sh
+++ kattest.sh
@@ -2,6 +2,8 @@
 
 files=$(find src/main/java/no/noria/edb/kattis -type f -name "*.java")
 
+status=0
+
 for f in $files; do
 
   # Prepare paths
@@ -12,30 +14,32 @@ for f in $files; do
   samples_zip="$samples_dir/samples.zip"
   samples_url="https://open.kattis.com/problems/$problem/file/statement/samples.zip"
 
-  if [[ $1 == "download" ]]; then
-    # Download samples if not already there
-    if [ ! -f $samples_zip ]; then
-      echo "  🎣 Downloading samples_url to $samples"
-      curl -s $samples_url > $samples_zip
-    fi
-
-    # Unzip samples_url
-    unzip -q -f $samples_zip -d $samples_dir
+  # Download samples if not already there
+  if [ ! -f $samples_zip ]; then
+    echo "  🎣 Downloading samples_url to $samples_zip"
+    curl -s $samples_url > $samples_zip
   fi
 
-    # Find input files
-    input_files=$(find $samples_dir -type f -name "*.in")
-    for input_file in $input_files; do
-      input=$(cat $input_file)
-      output_file=$(echo $input_file | sed 's/\.in/\.ans/')
-      output_file_actual="$output_file.actual"
-      output=$(cat $output_file)
-      echo "$input" | java $f > "$output_file_actual"
-      output_actual=$(cat "$output_file_actual")
-      if [[ "$output_actual" == "$output" ]]; then
-        echo "  ✅ $input_file"
-      else
-        echo "  ❌ $input_file"
-      fi
-    done
+  # Unzip samples_url
+  echo "  📦 Unzipping $samples_zip to $samples_dir"
+  unzip -q -o $samples_zip -d $samples_dir
+
+  # Find input files
+  input_files=$(find $samples_dir -type f -name "*.in")
+  for input_file in $input_files; do
+    input=$(cat $input_file)
+    output_file=$(echo $input_file | sed 's/\.in/\.ans/')
+    output_file_actual="$output_file.actual"
+    output=$(cat $output_file)
+    echo "$input" | java $f > "$output_file_actual"
+    output_actual=$(cat "$output_file_actual")
+    if [[ "$output_actual" == "$output" ]]; then
+      echo "  ✅ $input_file"
+    else
+      echo "  ❌ $input_file"
+      status=1
+    fi
+  done
 done
+
+exit $status
---
+++ src/main/java/no/noria/edb/kattis/Velkomin.java
@@ -0,0 +1,7 @@
+package no.noria.edb.kattis;
+
+public class Velkomin {
+    public static void main(String[] args) {
+        System.out.println("VELKOMIN!");
+    }
+}
